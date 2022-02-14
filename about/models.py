6c9d4d53518c059e558bf12122bd32ddd2771c8a
behdadhp <behdad.hajipour95@gmail.com>
2022-02-15 00:03:43 +0100

    creating submitted.html and views

--- about/models.py
+++ about/models.py
@@ -7,14 +7,14 @@ from djnago.contrib.auth import get_user_model
 User = get_user_model()
 
 class ContactUser(models.Model):
-    user = models.ForeignKey(User,related_name='usernames',blank=False,null=False,on_delete=models.CASCADE)
-    date = models.DateTimeField(default=datetime.now, blank=False, null=False)
-    email = models.EmailField(max_length=255, blank=False, null=False)
+    user    = models.ForeignKey(User,related_name='usernames',blank=False,null=False,on_delete=models.CASCADE)
+    date    = models.DateTimeField(default=datetime.now, blank=False, null=False)
+    email   = models.EmailField(max_length=255, blank=False, null=False)
     comment = models.CharField(max_length=1028, blank=False, null=False)
 
 
     def __str__(self):
         return self.user.username
 
-    # def get_absolute_url(self):
-    #     return reverse('')
+    def get_absolute_url(self):
+        return reverse("")
--- about/views.py
+++ about/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from . import models
+from django.views import generic
+from django.contrib.auth.models import LoginRequiredMixin
 # Create your views here.
+
+
+
+class SubmittedPage(generic.TemplateView, LoginRequiredMixin):
+    template_name = 'templates/submitted.html'
