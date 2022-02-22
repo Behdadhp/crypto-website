efce9c0b3afad816ce1476c8eb9a821d2e28b56e
behdadhp <behdad.hajipour95@gmail.com>
2022-02-22 22:05:14 +0100

    updated admin.py with class ContactUsAdmin

--- about/admin.py
+++ about/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from about.models import ContactUs
 # Register your models here.
 
-admin.site.register(ContactUs)
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display =('user','date','email','comment')
+    list_display_links = ('user','email')
+    ordering = ('-date',)
+
+
+admin.site.register(ContactUs, ContactUsAdmin)
