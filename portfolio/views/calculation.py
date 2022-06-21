510daec88df60cbca4a63a3b7ce46f7656cffd62
behdadhp <behdad.hajipour95@gmail.com>
2022-06-22 01:14:04 +0200

    created Asset class

--- portfolio/views/calculation.py
+++ portfolio/views/calculation.py
@@ -1,2 +1,9 @@
 from portfolio import models
 from market.api import data
+
+class Asset():
+
+    def __init__(self,models, user_req, pk_req):
+        self.query = models.objects
+        self.user_req = user_req
+        self.pk_req = pk_req
