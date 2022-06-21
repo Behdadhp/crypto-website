78fe070e899850cca093e9491d7347dc8bb1d0c3
behdadhp <behdad.hajipour95@gmail.com>
2022-06-22 01:14:36 +0200

    updated Asset with creating_query

--- portfolio/views/calculation.py
+++ portfolio/views/calculation.py
@@ -7,3 +7,8 @@ class Asset():
         self.query = models.objects
         self.user_req = user_req
         self.pk_req = pk_req
+
+    def creating_query(self):
+        queryset = self.query.filter(user_id = self.user_req,id=self.pk_req).values()
+
+        return queryset
