db9c4a467ae06f561e23255be665420fbc917d81
behdad hajipour <bhajipour@avodaq.com>
2022-12-24 13:10:44 +0100

    edited market

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,5 +5,3 @@ J
 crypto_website/settings.py,e\3\e391f7be7febc156c9fa5ff6836fb8581bb18eaa
 F
 crypto_website/urls.py,3\2\322d3c1433e06567e13d421336e6df8ef53e2908
-D
-templates/index.html,f\6\f69b8ce2e31684ae06f1b3e84821e07e3dfddb2f
--- market/views.py
+++ market/views.py
@@ -1,16 +1,21 @@
-from django.views.generic import (ListView, UpdateView)
+from django.views.generic import (UpdateView)
+from django_tables2 import SingleTableView
+
 from portfolio import models
 from .api import data
+from . import tables
 
 
-class MarketListView(ListView):
+class MarketListView(SingleTableView):
     context_object_name = 'markets'
     model = models.Market
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['data'] = data
-
+        table = tables.MarketTable(data)
+        table.paginate(page=self.request.GET.get("page", 1), per_page=15)
+        context["table"] = table
         return context
 
 
